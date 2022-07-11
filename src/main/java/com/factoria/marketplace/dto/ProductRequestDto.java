0fdb937458e6942783bbe1b64aa88c97eec125bb
[nboyaroca] <[nboyaroca@gmail.com]>
2022-07-11 23:53:33 +0200

    Test create saves a product mapped from dto

--- src/main/java/com/factoria/marketplace/dto/ProductRequestDto.java
+++ src/main/java/com/factoria/marketplace/dto/ProductRequestDto.java
@@ -1,8 +1,10 @@
 package com.factoria.marketplace.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ProductRequestDto {
     private String name;
     private String img;
--- src/test/java/com/factoria/marketplace/services/ProductServiceTest.java
+++ src/test/java/com/factoria/marketplace/services/ProductServiceTest.java
@@ -1,6 +1,8 @@
 package com.factoria.marketplace.services;
 
+import com.factoria.marketplace.dto.ProductRequestDto;
 import com.factoria.marketplace.models.Product;
+import com.factoria.marketplace.models.User;
 import com.factoria.marketplace.repositories.IProductRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -12,6 +14,7 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 class ProductServiceTest {
@@ -31,6 +34,24 @@ class ProductServiceTest {
     }
 
     @Test
-    void createProduct() {
+    void createSavesAProductMappedFromDTO() {
+        var productService = new ProductService(productRepository);
+
+        var productRequest = new ProductRequestDto("Product1", "img.jpg", 50.5);
+        var seller = new User();
+        seller.setId(1L);
+
+        var product = new Product();
+        product.setName("Product1");
+        product.setImg("img.jpg");
+        product.setPrice(50.5);
+        product.setId(1L);
+        product.setSeller(seller);
+
+        Mockito.when(productRepository.save(any(Product.class))).thenReturn(product);
+
+        var sut = productService.createProduct(productRequest, seller);
+
+        assertThat(sut.getSeller(), equalTo(seller));
     }
 }
